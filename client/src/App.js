1b3cbf7149eaa8905c11b021b75a97166c635c64
Christopher Yeom <chrisyeom97@gmail.com>
2022-10-31 17:50:45 -0400

    fixed the image

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import './App.css'
 import React from 'react'
-import Nav from './components/Nav'
-import AllCoasters from './components/AllCoasters'
+// import Nav from './components/Nav'
+// import AllCoasters from './components/AllCoasters'
 import CoasterRegions from './components/CoasterRegions'
-import Details from './components/Details'
-import Home from './components/Home'
-import NewCoasterForm from './components/NewCoasterForm'
+// import Details from './components/Details'
+// import Home from './components/Home'
+// import NewCoasterForm from './components/NewCoasterForm'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Nav />
-      </header>
+      <header>{/* <Nav /> */}</header>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/allcoasters" element={<AllCoasters />} />
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/allcoasters" element={<AllCoasters />} /> */}
           <Route path="/:id" element={<CoasterRegions />} />
-          <Route path="/coaster/:id" element={<Details />} />
-          <Route path="/newcoasterform" element={<NewCoasterForm />} />
+          {/* <Route path="/coaster/:id" element={<Details />} />
+          <Route path="/newcoasterform" element={<NewCoasterForm />} /> */}
         </Routes>
       </main>
     </div>
