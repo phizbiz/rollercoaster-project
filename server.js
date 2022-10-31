370b57f99207990b431351eeced51f740bc2fdbe
Philip Basnight <philipbasnight@gmail.com>
2022-10-31 15:10:30 -0400

    server.js updated with routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const db = require('./db')
 const logger = require('morgan')
 const cors = require('cors')
+const { Region, Rollercoaster } = require('./models')
 
 // require() imports and middleware here ^ ///////
 
@@ -14,6 +15,65 @@ app.use(express.json())
 app.use(logger('dev'))
 app.use(cors())
 // app.use() middleware here ^ ///////////////////
+
+//ROUTES
+
+// read all regions --> GET
+app.get('/regions', async (req, res) => {
+  let allRegions = await Region.find({})
+  res.json(allRegions)
+})
+
+// read all foods by region --> GET
+app.get('/region/:id', async (req, res) => {
+  const rollercoasterRegion = await Rollercoaster.find({
+    region: req.params.id
+  })
+  res.json(rollercoasterRegion)
+})
+
+// read one region --> GET
+app.get('/regions/:id', async (req, res) => {
+  let foundRegion = await Region.findById(req.params.id)
+  res.json(foundRegion)
+})
+
+// create regions --> POST
+app.post('/regions', async (req, res) => {
+  let createdRegion = await Region.create(req.body)
+  res.json(createdRegion)
+})
+
+// ROLLERCOASTER
+
+// read all foods --> GET
+app.get('/rollercoasters', async (req, res) => {
+  const allRollercoasters = await Rollercoaster.find({})
+  res.json(allRollercoasters)
+})
+
+// create food --> POST
+app.post('/rollercoasters', async (req, res) => {
+  let createdRollercoaster = await Rollercoaster.create(req.body)
+  res.json(createdRollercoaster)
+})
+
+// read one food --> GET
+app.get('/rollercoasters/:id', async (req, res) => {
+  let foundRollercoaster = await Rollercoaster.findById(req.params.id).populate(
+    'region'
+  )
+
+  res.json(foundRollercoaster)
+})
+
+// delete one food --> DELETE
+app.delete('/rollercoasters/:id', async (req, res) => {
+  let deletedRollercoaster = await Rollercoaster.findByIdAndDelete(
+    req.params.id
+  )
+  res.json(deletedRollercoaster)
+})
 
 app.use('/', routes)
 
