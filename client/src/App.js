6c2398003168b05ee432c4ef286f95eb8d406fdd
Philip Basnight <philipbasnight@gmail.com>
2022-10-31 22:54:29 -0400

    new coaster form added

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,7 @@ import { Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header>
-        <Nav />
-      </header>
+      <header>{/* <Nav /> */} </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
