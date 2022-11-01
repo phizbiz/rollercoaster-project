5a2e7860924b7e42e51024b534acb1a1fc7f0bbb
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-11-01 04:37:43 -0500

    Completed Home Component

---
+++ client/src/components/Home.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+
+
+const Home = () => {
+    const [regions, updateRegions] = useState([])
+
+    useEffect(() => {
+    const api = async () => {
+        let response = await axios.get('http://localhost:3001/regions')
+        updateRegions(response.data)
+    }
+    api()
+}, [])
+
+    return (
+
+            <div className='allRegions'>
+                <h1>Choose a Region:</h1>
+                {regions.map((region) => {
+                   return (
+                    <Link to={`/${region._id}`}>
+                    <div className="region" key={region._id}>
+                        <img src={region.image} />
+                        <h2>{region.name}</h2>
+                    </div>
+                    </Link>
+                   ) 
+                })}
+                <h3>hi</h3>
+            </div>
+        )
+  }
+  
+  export default Home
